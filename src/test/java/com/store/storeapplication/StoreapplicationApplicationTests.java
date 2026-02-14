befe08b42fd97f945e92e14e382d79735a60abc8
[Ahmad Warsi] <syedinitp@gmail.com>
2026-02-14 18:25:45 +0530

    Fixed CI database issue using H2

--- src/test/java/com/store/storeapplication/StoreapplicationApplicationTests.java
+++ src/test/java/com/store/storeapplication/StoreapplicationApplicationTests.java
@@ -2,8 +2,10 @@ package com.store.storeapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StoreapplicationApplicationTests {
 
 	@Test
